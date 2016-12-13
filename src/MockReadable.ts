104e8655041798222aedf8766fd8e405693df8b3
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-12-13 10:57:00 -0500

    fix(MockReadable): do not emit the same events more than once

--- src/MockReadable.ts
+++ src/MockReadable.ts
@@ -7,13 +7,14 @@ const defaults =
 
 export class MockReadable extends stream.Readable {
   private _data: Array<any> = [];
+  private _immutableData: Array<any> = [];
 
   constructor (options?: stream.ReadableOptions) {
     super(Object.assign({}, defaults, options));
   }
 
   protected _read (size?: number): void {
-    const data = this._data.slice(0);
+    const data = this._data;
 
     if (size === void 0) {
       size = data.length;
@@ -38,6 +39,7 @@ export class MockReadable extends stream.Readable {
     }
 
     this._data.push(...data);
+    this._immutableData.push(...data);
 
     this._read();
 
@@ -45,7 +47,7 @@ export class MockReadable extends stream.Readable {
   }
 
   public data (): Array<any> {
-    return this._data.slice(0);
+    return this._immutableData.slice(0);
   }
 
   public end (...args: any[]): void {
